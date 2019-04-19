744b524df533365ef45c041ff85d24dd2200e79f
Jeremy Gaultier <contact@webmezenc.com>
2019-04-19 10:50:37 +0200

    chore(composer): add annotations, framework-extra-bundle, form and validator

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
